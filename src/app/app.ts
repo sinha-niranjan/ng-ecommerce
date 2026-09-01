e68d565f9f640f2f93cf73958c4d492028841619
Niranjan Kumar <niranjankumaraina@gmail.com>
2026-09-01 23:44:35 +0530

    Initial commit : Project setup with Angular, TailwindCSS, and PostCSS configuration.

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Hello, {{ title() }}</h1>
+    <h1 class="text-2xl font-bold">Welcome to , {{ title() }}</h1>
 
     <router-outlet />
   `,
